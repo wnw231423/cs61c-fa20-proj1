17dc3ea6cec75fdf89120dd4a3b5800f34888e66
wnw231423 <2684722187@qq.com>
2024-09-18 22:57:28 +0800

    Part A finished.

--- imageloader.c
+++ imageloader.c
@@ -7,7 +7,7 @@
 ** AUTHOR:      Dan Garcia  -  University of California at Berkeley
 **              Copyright (C) Dan Garcia, 2020. All rights reserved.
 **              Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				wnw231423
 **
 **
 ** DATE:        2020-08-15
@@ -25,17 +25,57 @@
 //Make sure that you close the file with fclose before returning.
 Image *readData(char *filename) 
 {
-	//YOUR CODE HERE
+	FILE *fp = fopen(filename, "r");
+	Image *res = (Image*) malloc(sizeof(Image));
+
+	char str[20];
+	int m, n, r, g, b;
+	fscanf(fp, "%s", str);
+	fscanf(fp, "%d%d", &m, &n);
+	res->cols = m;
+	res->rows = n;
+
+	res->image = (Color**) malloc(n * sizeof(Color*));
+	for (int i = 0; i < n; i++) {
+		res->image[i] = (Color*) malloc(m * sizeof(Color*));
+	}
+
+	fscanf(fp, "%d", &r);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			fscanf(fp, "%d%d%d", &r, &g, &b);
+			Color node;
+			node.R = r;
+			node.G = g;
+			node.B = b;
+			res->image[i][j] = node;
+		}
+	}
+	fclose(fp);
+	return res;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+	printf("P3\n");
+	printf("%d %d\n", image->cols, image->rows);
+	printf("255\n");
+	for (int i = 0; i < image->rows; i++) {
+		for (int j = 0; j < image->cols; j++) {
+			if (j != 0) {
+				printf("   ");
+			}
+			printf("%3d %3d %3d", image->image[i][j].R, image->image[i][j].G, image->image[i][j].B);
+			if (j == image->rows - 1) {
+				printf("\n");
+			}
+		}
+	}
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
-	//YOUR CODE HERE
+	free(image);
 }
